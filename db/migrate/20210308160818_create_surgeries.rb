d7958901087c7366536d11cb6e641eb7c87927cd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-08 11:09:10 -0500

    Create migrations for doctors and surguries

---
+++ db/migrate/20210308160818_create_surgeries.rb
@@ -0,0 +1,11 @@
+class CreateSurgeries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :surgeries do |t|
+      t.string :title
+      t.integer :week_day
+      t.integer :operating_room
+
+      t.timestamps
+    end
+  end
+end
